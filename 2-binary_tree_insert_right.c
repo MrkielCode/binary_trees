acd4b3895e9ee32e59ca8da6d25e9fa791869e78
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-01 14:10:07 +0100

    shift right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent->right != NULL)
 	{
-		new_node = parent->right;
+		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
 
