109772227db59fa7655545808a41f484982195c8
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-02 16:55:41 +0100

    all

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,20 +8,15 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-
 	if (node == NULL ||
-			node->parent == NULL
-			|| node->parent->parent == NULL)
+			node->parent == NULL ||
+			node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
-	if (parent->parent->left == node->parent)
+	if (node->parent->parent->left == node->parent)
 	{
-		return (parent->parent->right);
+		return (node->parent->parent->right);
 	}
-	else
-	{
-		return (parent->parent->left);
-	}
+	return (node->parent->parent->left);
 }
