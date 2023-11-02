4da71e4c6d40dfaaa9dd050469d6f7f18d8d1416
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-02 13:54:18 +0100

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,10 +25,10 @@ int tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
-
-	int left_height, right_height;
 
 	left_height = tree_height(tree->left);
 	right_height = tree_height(tree->right);
